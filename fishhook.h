5729f374e4105366313e79426ee2772169e30f04
max44009933-design <max44009933@gmail.com>
2026-03-19 12:32:52 +0800

    Update fishhook.h

--- fishhook.h
+++ fishhook.h
@@ -1,7 +1,19 @@
 #ifndef fishhook_h
 #define fishhook_h
+
 #include <stddef.h>
 #include <stdint.h>
+
+// 🌟 核心防護：只包住函數，避開系統庫！
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct rebind_msg { const char *name; void *replacement; void **replaced; };
 int rebind_symbols(struct rebind_msg *rebinds, size_t rebinds_nel);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
